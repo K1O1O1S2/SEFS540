ee102b76fbdfd1294be917360c9225c69b380b07
K1O1O1S2 <clemence.labarre@gmail.com>
2022-07-23 10:17:32 +0200

    Write_file_Yhp00constraint_and_X-Y_bounds

---
+++ Deer_hab/Max-pulp.sol
@@ -0,0 +1,24 @@
+<?xml version = "1.0" encoding="UTF-8" standalone="yes"?>
+<CPLEXSolution version="1.2">
+ <header
+   problemName="Max-pulp.lp"
+   objectiveValue="430"
+   solutionTypeValue="1"
+   solutionTypeString="basic"
+   solutionStatusValue="1"
+   solutionStatusString="optimal"
+   solutionMethodString="dual"
+   primalFeasible="1"
+   dualFeasible="1"
+   simplexIterations="0"
+   writeLevel="1"/>
+ <quality
+   epRHS="9.9999999999999995e-07"
+   epOpt="9.9999999999999995e-07"
+   maxPrimalInfeas="0"
+   maxDualInfeas="0"
+   maxPrimalResidual="1.0089706847793423e-12"
+   maxDualResidual="0"
+   maxX="24901"
+   maxPi="0"
+  
